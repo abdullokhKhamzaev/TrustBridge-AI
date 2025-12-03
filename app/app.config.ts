c08f375104b001476008ae81c9aa5740d57ecf90
Abdullokh Khamzaev <abdullokh.khamzaev@gmail.com>
2025-12-03 23:02:20 +0500

    Optimizing UI Config

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,31 @@ export default defineAppConfig({
     colors: {
       primary: 'cyan',
       neutral: 'slate'
+    },
+    button: {
+      slots: {
+        base: 'cursor-pointer'
+      }
+    },
+    input: {
+      slots: {
+        root: 'w-full relative inline-flex items-center'
+      }
+    },
+    textarea: {
+      slots: {
+        root: 'w-full relative'
+      }
+    },
+    select: {
+      slots: {
+        base: 'w-full relative'
+      }
+    },
+    selectMenu: {
+      slots: {
+        base: 'w-full relative'
+      }
     }
   }
 })
